79f909b59a0f1dfdcef3790d2876937f8e0e39ce
AIBigPlayer <leee1hui@gmail.com>
2024-08-16 16:02:45 +0800

    Site updated: 2024-08-16 16:02:44

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2024/03/06/how-to-pay-for-chatgpt-team/","2024/01/31/how-to-pay-for-chatgpt4/","2024/02/23/how-to-regist-chatgpt/","2024/02/27/how-to-regist-onlyfans/","2024/08/13/how-to-pay-for-claude-pro/","2024/02/21/how-to-use-sora/","2024/03/18/onlyfans/how-to-subscribe-for-onlyfans/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
