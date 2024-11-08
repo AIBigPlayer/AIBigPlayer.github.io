6c9100e68568e273695c8a5090acc0f07fab7c0c
AIBigPlayer <leee1hui@gmail.com>
2024-11-08 15:46:19 +0800

    Site updated: 2024-11-08 15:46:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["how-to-pay-for-chatgpt4/","how-to-pay-for-chatgpt-team/","How-to-Use-Virtual-Credit-Cards/","how-to-pay-for-claude-pro/","how-to-regist-chatgpt/","how-to-use-sora/","how-to-use-chatgpt-search/"];function toRandomPost(){
+var posts=["how-to-pay-for-chatgpt4/","how-to-pay-for-chatgpt-team/","How-to-Use-Virtual-Credit-Cards/","how-to-pay-for-claude-pro/","how-to-regist-chatgpt/","how-to-use-sora/","how-to-use-chatgpt-search/","chatgpt-search-travel-guide/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
