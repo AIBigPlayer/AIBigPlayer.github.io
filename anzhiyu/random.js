2e3df4a90ff9c4d8aca2a83bded1863b9d2e25b7
AIBigPlayer <leee1hui@gmail.com>
2024-11-17 16:21:35 +0800

    Site updated: 2024-11-17 16:21:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/2024/08/19/How-to-Use-Virtual-Credit-Cards/","chatgpt-search-travel-guide/","/2024/08/13/how-to-pay-for-claude-pro/","/2024/01/31/how-to-pay-for-chatgpt4/","/2024/03/06/how-to-pay-for-chatgpt-team/","/2024/02/23/how-to-regist-chatgpt/","how-to-use-chatgpt-search/","/2024/02/21/how-to-use-sora/","chatgpt-share/","bewildcard/"];function toRandomPost(){
+var posts=["/2024/08/19/How-to-Use-Virtual-Credit-Cards/","chatgpt-search-travel-guide/","/2024/08/13/how-to-pay-for-claude-pro/","/2024/01/31/how-to-pay-for-chatgpt4/","/2024/03/06/how-to-pay-for-chatgpt-team/","/2024/02/23/how-to-regist-chatgpt/","how-to-use-chatgpt-search/","/2024/02/21/how-to-use-sora/","chatgpt-share/","bewildcard/","claude-share/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
